a4d255a4ecc83dc747b8655b248576e75f877521
swdrana <tctr2s@gmail.com>
2022-05-12 14:31:59 +0600

    update page styled

--- src/components/Update/Update.js
+++ src/components/Update/Update.js
@@ -3,6 +3,13 @@ import { Button, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Product from "../Product/Product";
 import "./Update.css";
+import {RiUserReceivedLine} from 'react-icons/ri'
+import {HiOutlineMail} from 'react-icons/hi'
+import {MdPersonOutline, MdProductionQuantityLimits} from 'react-icons/md'
+import {FcAddImage, FcCurrencyExchange, FcMultipleDevices} from 'react-icons/fc'
+import {BsPencilSquare} from 'react-icons/bs'
+
+
 const Update = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
@@ -51,7 +58,7 @@ const Update = () => {
       quantity = quantityInNumber;
       console.log(quantityInNumber);
 
-      const updatedProduct = {_id, name, email, imgLink, quantity, pricePerItem, supplierName, description, };
+      const updatedProduct = {_id, name, email, productName, quantity, pricePerItem, supplierName, description, imgLink};
       // send updated product to database
       fetch(`http://localhost:8080/update/${id}`, {
         method: "PUT",
@@ -68,45 +75,54 @@ const Update = () => {
   }
   return (
     <div className="mx-auto my-5 container">
-        <h2 className="text-center">Welcome To Update Page!</h2>
+        <h2 className="text-center my-5">📝 Welcome To Update Page! 📝</h2>
         <div className="row d-flex align-items-center">
 
         <Product key={product._id} product={product}></Product>
         {console.log(product)}
-      <Form onSubmit={handelUpdate} className="col-md-8 mt-5 w-50">
-        <Form.Group className="mb-3" controlId="formBasicName">
+      <Form onSubmit={handelUpdate} className="col-md-8 mt-5 px-5 mx-auto">
+        <Form.Group className="mb-2 d-flex" controlId="formBasicName">
+            <RiUserReceivedLine size="35px" className="me-4" color="gray"/>
           <Form.Control type="text" placeholder="Name" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="newEmail">
+        <Form.Group className="mb-2 d-flex" controlId="newEmail">
+            <HiOutlineMail size="35px" className="me-4" color="gray"/>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="productName">
+        <Form.Group className="mb-2 d-flex" controlId="productName">
+            <FcMultipleDevices size="35px" className="me-4" color="black"/>
           <Form.Control type="text" placeholder="Product Name" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicQuantity">
+        <Form.Group className="mb-2 d-flex" controlId="formBasicQuantity">
+            <MdProductionQuantityLimits size="35px" className="me-4" color="red"/>
           <Form.Control type="number" placeholder="Quantity" min="1" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="pricePerItem">
+        <Form.Group className="mb-2 d-flex" controlId="pricePerItem">
+            <FcCurrencyExchange size="35px" className="me-4" color="black"/>
           <Form.Control type="number" placeholder="Price of each item" min="0" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicSupplier">
+        <Form.Group className="mb-2 d-flex" controlId="formBasicSupplier">
+            <MdPersonOutline size="35px" className="me-4" color="green"/>
           <Form.Control type="text" placeholder="Supplier Name" />
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-2 d-flex">
+            <BsPencilSquare size="35px" className="me-4" color="grey"/>
           <textarea
             name=""
             id="formBasicDescription"
             style={{ width: "100%", border: "1px solid #ced4da" }}
             rows="4"
-            placeholder="Description"
+            placeholder=" Description"
+            className="ps-2"
           ></textarea>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="imgLink">
+        <Form.Group className="mb-2 d-flex" controlId="imgLink">
+            <FcAddImage size="35px" className="me-4" color="black"/>
           <Form.Control type="text" placeholder="Image Link" />
         </Form.Group>
         <div className="btn d-flex justify-content-between w-50 mx-auto">
           <Button variant="success" type="submit">
-            Update
+            Update Changes
           </Button>
           <Button variant="danger" onClick={handelDelivered}>Delivered</Button>
         </div>
