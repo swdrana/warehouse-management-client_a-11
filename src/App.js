1f7025b3fdaba26ff1fa3ae60cac66e3210ab24b
swdrana <tctr2s@gmail.com>
2022-05-13 19:02:18 +0600

    404 added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Blogs from './components/Blogs/Blogs';
 import Footer from './components/Footer/Footer';
 import Update from './components/Update/Update';
 import Signup from './Pages/Signup/Signup';
+import NotFound from './components/NotFound/NotFound';
 import RequireAuth from './Pages/RequireAuth/RequireAuth';
 import { ToastContainer } from 'react-toastify';
 
@@ -30,6 +31,7 @@ function App() {
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/update/:id' element={<RequireAuth><Update/></RequireAuth>}/>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
       <Footer/>
     </div>
